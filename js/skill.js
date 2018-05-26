63ac55aa4f4014644e3d3f541b06b0ee0f5835a7
Chi-Hsin <jack1234552000@yahoo.com.tw>
2018-05-26 15:10:09 +0800

    Site updated: 2018-05-26 15:05:56

--- js/skill.js
+++ js/skill.js
@@ -1,21 +1,15 @@
 $(function(){
-	$("a.skill_name").each(function(){
-		var text= $(this).text();
-		$(this).css("display","inline-block").css("color","blue")
-		.attr("href","#"+text);
+	$(".skill_note").each(function(){      //底下兩個this分別指向不同的對象
+		$(this).find("a").each(function(){ //技能說明處找出底下有超連結標籤的
+			$(this).attr({"class":"skill_name","href":"#"+$(this).text()});//所有相關的超連結賦予連結
+		})
 	})
+
 	$("h1:not(h1:first)").hover(function(){ //排除第一個標題的情況
 		var skill_name = $(this).text();
 		function note_word() //提醒文字
 		{
 			var obj = document.createElement("span");
-			obj.style.background = "rgb(51,122,183)";
-			obj.style.color = "#fff";
-			obj.style.width = "300px";
-			obj.style.height = "25px";
-			obj.style.fontSize = "15px";
-			obj.style.marginLeft = "10px";
-			obj.style.textAlign = "center";
 			obj.textContent = "點擊查看擁有"+skill_name+"的卡片";
 			return obj;
 		}
