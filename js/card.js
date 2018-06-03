8ef32395766278ad39c4023500ed6514296c6ca9
Chi-Hsin <jack1234552000@yahoo.com.tw>
2018-06-03 21:59:11 +0800

    Site updated: 2018-06-03 21:57:02

---
+++ js/card.js
@@ -0,0 +1,22 @@
+$(function(){
+	var chart = c3.generate({
+	    bindto: '#mychart',
+	    data: {
+	      columns: [
+	         ['技能1', 0, 0, 0, 0, 0,0,1, 1],
+	        ['技能2', 1, 1, 2, 2, 3,3,3,4],
+	        ['技能3', 7, 8, 9, 9, 10,9,10, 12]
+	      ]
+	    },
+	    axis: {
+	        y: {
+	            padding: {bottom: 0},
+	            min: 0
+	        },
+	        x: {
+	            padding: {left: 0},
+	            min: 0
+	        }
+	    }
+	});
+})
